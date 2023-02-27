4cef22fad8603116fc7eed2a894c4cd064b8e7c2
Victor Tan <victor.tan.33@gmail.com>
2023-02-27 07:56:19 +0000

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("oh my god this workshop is sooooo boring !!!!!");
+        System.out.println("this workshop has suddenly become interesting again !!!");
     }
 
     public String getMessage() {
