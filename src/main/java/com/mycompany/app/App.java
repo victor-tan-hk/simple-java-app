045cbd80c7c3228917ac39fd8591e987b6654525
Victor Tan <victor.tan.33@gmail.com>
2023-02-28 05:38:38 +0000

    Made it all ok now

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "I really really HATEEEEEEEE Jenkins";
+    private static final String MESSAGE = "I love Jenkins";
 
     public App() {}
 
