6f85bdd8f1c8f01d1453d638ba0f5706f53a572d
Victor Tan <victor.tan.33@gmail.com>
2024-02-21 14:14:22 +0800

    Some random change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("THis is a normal change");
+        System.out.println("This is my latest change. So cool !!!!");
     }
 
     public String getMessage() {
