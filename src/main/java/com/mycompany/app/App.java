a4d2ebfe35e8c2fc2a07eee590aa22d11380d0f3
Victor Tan <victor.tan.33@gmail.com>
2024-02-21 09:24:05 +0800

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Changing to demo periodic polling");
+        System.out.println("Testing out webhook feature");
     }
 
     public String getMessage() {
